2f10ef1fe0bad91dffdd7156248e9d9fcf280f68
dfreire <dario.freire@gmail.com>
2016-04-05 21:59:32 +0100

    update

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/jmoiron/sqlx"
+    _ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+    fmt.Println("Hello")
+
+    var db *sqlx.DB
+
+    db = sqlx.Open("sqlite3", ":memory:")
+    db = sqlx.NewDB(sql.Open("sqlite3", ":memory:"), "sqlite3")
+    err = db.Ping()
+}
