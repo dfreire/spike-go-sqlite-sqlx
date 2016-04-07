c4eb29c1bf4f7d5b654b2b33e4234acf3f4e90c5
dfreire <dario.freire@gmail.com>
2016-04-07 20:13:27 +0100

    update

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"log"
 	"os"
+
+	"gopkg.in/guregu/null.v3"
+
+	// "github.com/jmoiron/sqlx"
+	// "gopkg.in/jmoiron/sqlx.sqlx-v1.1"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	schema := `CREATE TABLE place (
-        country text,
-        city text NULL,
-        telcode integer
+	schema := `CREATE TABLE user (
+        name text,
+        birth_date text NULL,
+        birth_place text NULL
     );`
 
 	db, err := sqlx.Open("sqlite3", ":memory:")
@@ -27,20 +31,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	countryTelcode := `INSERT INTO place (country, telcode) VALUES (?, ?)`
-	countryAll := `INSERT INTO place (country, city, telcode) VALUES (?, ?, ?)`
+	userBirthPlace := `INSERT INTO user (name, birth_date) VALUES (?, ?)`
+	userAll := `INSERT INTO user (name, birth_date, birth_place) VALUES (?, ?, ?)`
 
-	db.MustExec(countryTelcode, "Hong Kong", 852)
-	db.MustExec(countryTelcode, "Singapore", 65)
-	db.MustExec(countryAll, "South Africa", "Johannesburg", 27)
+	db.MustExec(userBirthPlace, "Mark", "New York")
+	db.MustExec(userBirthPlace, "Karl", "San Francisco")
+	db.MustExec(userAll, "Helen", "1983-03-13 12:23:32.871", "Johannesburg")
 
-	type Place struct {
-		Country string
-		City    sql.NullString
-		Telcode int
+	type User struct {
+		Name       string
+		BirthDate  null.String `db:"birth_date"`
+		BirthPlace null.String `db:"birth_place"`
 	}
 
-	rows, err := db.Queryx("SELECT * FROM place")
+	rows, err := db.Queryx("SELECT * FROM user")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,13 +52,9 @@ func main() {
 	enc := json.NewEncoder(os.Stdout)
 
 	for rows.Next() {
-		var p Place
-		err = rows.StructScan(&p)
-		enc.Encode(p)
-
-		m := make(map[string]interface{})
-		err = rows.MapScan(m)
-		enc.Encode(m)
+		var u User
+		err = rows.StructScan(&u)
+		enc.Encode(u)
 	}
 
 }
