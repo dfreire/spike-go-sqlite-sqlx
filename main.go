db60cbbf1242ab88c5997a7bdc139490e2ac9902
dfreire <dario.freire@gmail.com>
2016-04-09 10:30:25 +0100

    update

--- main.go
+++ main.go
@@ -1,84 +1,92 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"os"
-	"strings"
+// "github.com/jmoiron/sqlx"
+// "gopkg.in/jmoiron/sqlx.sqlx-v1.1"
 
-	"gopkg.in/guregu/null.v3"
+// _ "github.com/mattn/go-sqlite3"
 
-	// "github.com/jmoiron/sqlx"
-	// "gopkg.in/jmoiron/sqlx.sqlx-v1.1"
+type Record map[string]interface{}
 
-	"github.com/jmoiron/sqlx"
-	_ "github.com/mattn/go-sqlite3"
-)
+func InsertRecord(tableName string, record Record) {
+
+}
+
+func UpdateRecordsById(tableName string, ids []string, record Record) {
+
+}
+
+func DeleteRecordsById(tableName string, ids []string) {
+
+}
+
+const SCHEMA = `CREATE TABLE book (
+	id     TEXT PRIMARY KEY,
+	title  TEXT,
+	year   NUMBER
+);`
+
+type Book struct {
+	ID    string
+	Title string
+	year  int
+}
 
 func main() {
-	schema := `CREATE TABLE user (
-		id          INTEGER PRIMARY KEY AUTOINCREMENT,
-        name        TEXT,
-        birth_date  TEXT NULL,
-        birth_place TEXT NULL
-    );`
-
-	db, err := sqlx.Open("sqlite3", ":memory:")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = db.Exec(schema)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	userBirthPlace := `INSERT INTO user (name, birth_place) VALUES (?, ?)`
-	userAll := `INSERT INTO user (name, birth_date, birth_place) VALUES (?, ?, ?)`
-
-	db.MustExec(userBirthPlace, "Mark", "New York")
-	db.MustExec(userBirthPlace, "Karl", "San Francisco")
-	db.MustExec(userAll, "Helen", "1983-03-13 12:23:32.871", "Johannesburg")
-
-	type User struct {
-		Id         int
-		Name       string
-		BirthDate  null.String `db:"birth_date"`
-		BirthPlace null.String `db:"birth_place"`
-	}
-
-	rows, err := db.Queryx("SELECT * FROM user")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	enc := json.NewEncoder(os.Stdout)
-
-	for rows.Next() {
-		var u User
-		err = rows.StructScan(&u)
-		enc.Encode(u)
-	}
-
-	s := []string{}
-	s = append(s, "UPDATE user SET")
-
-	m := make(map[string]interface{})
-	m["birth_place"] = "London"
-
-	for key, value := range m {
-		s = append(s, fmt.Sprintf("%s = '%+v'", key, value))
-	}
-
-	s = append(s, "WHERE id IN (?, ?, ?)")
-
-	db.MustExec(strings.Join(s, " "), 1, 2, 3)
-
-	rows, err = db.Queryx("SELECT * FROM user")
-	for rows.Next() {
-		var u User
-		err = rows.StructScan(&u)
-		enc.Encode(u)
-	}
+	// db, err := sqlx.Open("sqlite3", ":memory:")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// _, err = db.Exec(schema)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// userBirthPlace := `INSERT INTO user (name, birth_place) VALUES (?, ?)`
+	// userAll := `INSERT INTO user (name, birth_date, birth_place) VALUES (?, ?, ?)`
+	//
+	// db.MustExec(userBirthPlace, "Mark", "New York")
+	// db.MustExec(userBirthPlace, "Karl", "San Francisco")
+	// db.MustExec(userAll, "Helen", "1983-03-13 12:23:32.871", "Johannesburg")
+	//
+	// type User struct {
+	// 	Id         int
+	// 	Name       string
+	// 	BirthDate  null.String `db:"birth_date"`
+	// 	BirthPlace null.String `db:"birth_place"`
+	// }
+	//
+	// rows, err := db.Queryx("SELECT * FROM user")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// enc := json.NewEncoder(os.Stdout)
+	//
+	// for rows.Next() {
+	// 	var u User
+	// 	err = rows.StructScan(&u)
+	// 	enc.Encode(u)
+	// }
+	//
+	// s := []string{}
+	// s = append(s, "UPDATE user SET")
+	//
+	// m := make(map[string]interface{})
+	// m["birth_place"] = "London"
+	//
+	// for key, value := range m {
+	// 	s = append(s, fmt.Sprintf("%s = '%+v'", key, value))
+	// }
+	//
+	// s = append(s, "WHERE id IN (?, ?, ?)")
+	//
+	// db.MustExec(strings.Join(s, " "), 1, 2, 3)
+	//
+	// rows, err = db.Queryx("SELECT * FROM user")
+	// for rows.Next() {
+	// 	var u User
+	// 	err = rows.StructScan(&u)
+	// 	enc.Encode(u)
+	// }
 }
